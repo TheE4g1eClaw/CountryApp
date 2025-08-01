4e78efae0ad560084178314f279eb0c637164955
TheE4g1eClaw <Enrique.Hernandez2@solera.com>
2025-07-31 21:44:10 -0600

    Added planet gif and theme changes

--- app/src/main/java/com/solera/countryapp/MainActivity.kt
+++ app/src/main/java/com/solera/countryapp/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.solera.countryapp
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
 import com.solera.countryapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,5 +17,11 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val imageView = findViewById<ImageView>(binding.worldImg.id)
+        Glide.with(this)
+            .asGif()
+            .load(R.drawable.planet_gif)
+            .into(imageView)
     }
 }
