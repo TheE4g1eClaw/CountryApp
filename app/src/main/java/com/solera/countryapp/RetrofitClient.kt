d2d3b325207eb4f0a609cbbe485b2b928e9709fd
jalberto206 <josealberto.cruz@solera.com>
2025-07-31 22:12:43 -0600

    Country Response gradle client post merge interface

--- app/src/main/java/com/solera/countryapp/RetrofitClient.kt
+++ app/src/main/java/com/solera/countryapp/RetrofitClient.kt
@@ -1,12 +1,15 @@
 package com.solera.countryapp
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object RetrofitClient {
     private const val BASE_URL="https://www.apicountries.com/countries"
-    val service:RickAndMortyService by lazy {
+    val service:CountryService by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-            .create(RickAndMortyService::class.java)
+            .create(CountryService::class.java)
     }
 }
