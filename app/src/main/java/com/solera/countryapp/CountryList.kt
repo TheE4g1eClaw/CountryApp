ac7b1390eb07855876b4336dbbb9ccfe84b29db2
Ivette Godinez <nadiaivette.godinez@solera.com>
2025-07-31 23:31:24 -0600

    Updating CountryList and activity_country_list.xml

--- app/src/main/java/com/solera/countryapp/CountryList.kt
+++ app/src/main/java/com/solera/countryapp/CountryList.kt
@@ -1,14 +1,11 @@
 package com.solera.countryapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class CountryList : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         }
     }
-}
