f1cb6bbcc6031d8937c8f7e11367cb41886a38bc
TheE4g1eClaw <Enrique.Hernandez2@solera.com>
2025-08-01 11:50:56 -0600

    Add country and region labels, update layout for country list items, and implement flag image loading

--- app/src/main/java/com/solera/countryapp/RetrofitClient.kt
+++ app/src/main/java/com/solera/countryapp/RetrofitClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL="https://www.apicountries.com/"
+    private const val BASE_URL="https://restcountries.com/v3.1/"
     val service:CountryService by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
